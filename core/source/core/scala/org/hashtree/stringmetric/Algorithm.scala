434df013728807dad4967391bec128f78ad25194
Rocky Madden <git@rockymadden.com>
2012-10-28 17:55:48 -0600

    Added proper covarience. Whoops.

--- core/source/core/scala/org/hashtree/stringmetric/Algorithm.scala
+++ core/source/core/scala/org/hashtree/stringmetric/Algorithm.scala
@@ -1,6 +1,6 @@
 package org.hashtree.stringmetric
 
 /** Marks those which leverage traits of a stand alone algorithm. */
-trait Algorithm[T, C] {
-	def compute(t: T)(implicit c: C): Option[T]
+trait Algorithm[T, F <: Filter[T]] {
+	def compute(t: T)(implicit f: F): Option[T]
 }
