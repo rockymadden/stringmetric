ec47080bda4ca3b79a14bd0eb77a2bbad6c18612
Rocky Madden <git@rockymadden.com>
2012-10-31 13:27:52 -0600

    Removed unused implicit val.

--- cli/source/core/scala/org/hashtree/stringmetric/cli/phonetic/package.scala
+++ cli/source/core/scala/org/hashtree/stringmetric/cli/phonetic/package.scala
@@ -2,5 +2,5 @@ package org.hashtree.stringmetric.cli
 
 /** Provides core phonetic CLI functionality. */
 package object phonetic {
-	implicit val optionMap: OptionMap = OptionMapUtility.toOptionMap(Array("--unitTest=false"))
+
 }
--- cli/source/core/scala/org/hashtree/stringmetric/cli/similarity/package.scala
+++ cli/source/core/scala/org/hashtree/stringmetric/cli/similarity/package.scala
@@ -2,5 +2,5 @@ package org.hashtree.stringmetric.cli
 
 /** Provides core similarity CLI functionality. */
 package object similarity {
-	implicit val optionMap: OptionMap = OptionMapUtility.toOptionMap(Array("--unitTest=false"))
+
 }
