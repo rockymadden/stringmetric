e347105de2d80527592533bc060d53de09c7fb78
Rocky Madden <git@rockymadden.com>
2012-11-19 12:24:36 -0700

    Expanded methods and created spec.

--- core/source/core/scala/org/hashtree/stringmetric/phonetic/Alphabet.scala
+++ core/source/core/scala/org/hashtree/stringmetric/phonetic/Alphabet.scala
@@ -1,10 +1,27 @@
 package org.hashtree.stringmetric.phonetic
 
+import scala.annotation.tailrec
+
 object Alphabet {
 	final val SometimesVowels: Set[Char] = Set('a', 'e', 'i', 'o', 'u', 'y')
 	final val Vowels: Set[Char] = Set('a', 'e', 'i', 'o', 'u')
 
-	def is(char: Char) = ((char >= 65 && char <= 90) || (char >= 97 && char <= 122))
+	def is(char: Char): Boolean = (char >= 65 && char <= 90) || (char >= 97 && char <= 122)
+
+	def is(charArray: Array[Char]): Boolean = {
+		@tailrec
+		def still(ca: Seq[Char]): Boolean =
+			if (ca.length == 0) true
+			else
+				if (!is(ca.head)) false
+				else still(ca.tail)
+
+		if (charArray.length == 0) false
+		else if (charArray.length <= 4) still(charArray)
+		else still(charArray.toList)
+	}
+
+	def is(string: String): Boolean = is(string.toCharArray)
 
 	def isSometimesVowel(char: Char): Boolean = (char == 'y' || char == 'Y' || isVowel(char))
 
