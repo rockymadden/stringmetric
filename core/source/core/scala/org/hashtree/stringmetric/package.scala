f86519c3b243d27d6f53e8330a8d7793bb95a0ed
Rocky Madden <git@rockymadden.com>
2012-11-27 14:07:52 -0700

    Removed unneeded import.

--- core/source/core/scala/org/hashtree/stringmetric/package.scala
+++ core/source/core/scala/org/hashtree/stringmetric/package.scala
@@ -1,7 +1,5 @@
 package org.hashtree
 
-import org.hashtree.stringmetric.filter.StringFilterDelegate
-
 /** Provides core string metric functionality. */
 package object stringmetric {
 	type CompareTuple[T] = Tuple2[Array[T], Array[T]]
