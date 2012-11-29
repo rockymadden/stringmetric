8620dddf970aafe1382551e79e2e83928e99ae37
Rocky Madden <git@rockymadden.com>
2012-11-29 07:50:26 -0700

    Mixed in ScalaTest BeforeAndAfter trait.

--- core/source/test/scala/org/hashtree/stringmetric/ScalaTest.scala
+++ core/source/test/scala/org/hashtree/stringmetric/ScalaTest.scala
@@ -1,9 +1,9 @@
 package org.hashtree.stringmetric
 
-import org.scalatest.WordSpec
+import org.scalatest.{ BeforeAndAfter, WordSpec }
 import org.scalatest.matchers.ShouldMatchers
 
-trait ScalaTest extends WordSpec with ShouldMatchers {
+trait ScalaTest extends WordSpec with ShouldMatchers with BeforeAndAfter {
 	def allows = afterWord("allow")
 
 	def executes = afterWord("execute")
