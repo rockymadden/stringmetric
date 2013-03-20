0f7c23e07bac35b63137b182aaa23aaf3a55e93a
Rocky Madden <git@rockymadden.com>
2013-03-19 18:28:13 -0600

    Removed empty package objects.

--- cli/source/core/scala/com/rockymadden/stringmetric/cli/phonetic/package.scala
+++
@@ -1,6 +0,0 @@
-package com.rockymadden.stringmetric.cli
-
-/** Provides core phonetic CLI functionality. */
-package object phonetic {
-
-}
--- cli/source/core/scala/com/rockymadden/stringmetric/cli/similarity/package.scala
+++
@@ -1,6 +0,0 @@
-package com.rockymadden.stringmetric.cli
-
-/** Provides core similarity CLI functionality. */
-package object similarity {
-
-}
