e4a66e7745aada3118a6e0261e00ece5c10b5893
Rocky Madden <git@rockymadden.com>
2013-03-19 18:34:52 -0600

    More restrictive return type.

--- core/source/core/scala/com/rockymadden/stringmetric/StringAlgorithm.scala
+++ core/source/core/scala/com/rockymadden/stringmetric/StringAlgorithm.scala
@@ -1,7 +1,7 @@
 package com.rockymadden.stringmetric
 
 trait StringAlgorithm[A, B] extends Algorithm[String, A, B] {
-	def compute(charArray: Array[Char])(implicit a: A): Option[Array[_]]
+	def compute(charArray: Array[Char])(implicit a: A): Option[Array[Char]]
 }
 
 object StringAlgorithm {
