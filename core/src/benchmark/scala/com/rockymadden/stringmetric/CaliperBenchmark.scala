2dcccf22d03f116a139ebd98dc803c02d860367d
Rocky Madden <git@rockymadden.com>
2014-01-05 10:52:42 -0700

    Removed benchmarks as it is outdated and needs an overhaul.

--- core/src/benchmark/scala/com/rockymadden/stringmetric/CaliperBenchmark.scala
+++
@@ -1,7 +0,0 @@
-package com.rockymadden.stringmetric
-
-import com.google.caliper.SimpleBenchmark
-
-trait CaliperBenchmark extends SimpleBenchmark {
-	def run(reps: Int)(code: => Unit) = (0 until reps).foreach(i => code)
-}
--- core/src/benchmark/scala/com/rockymadden/stringmetric/CaliperRunner.scala
+++
@@ -1,7 +0,0 @@
-package com.rockymadden.stringmetric
-
-import com.google.caliper.{Benchmark, Runner}
-
-abstract class CaliperRunner(private[this] val suite: java.lang.Class[_ <: Benchmark]) {
-	def main(args: Array[String]): Unit = Runner.main(suite, args)
-}
